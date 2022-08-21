ac341c0d4523ebe7be31e664d038c7ac8b9f81cc
Oleksandr Kholod <avxolod@gmail.com>
2022-08-21 22:48:40 +0300

    done task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+function loadGallery() {
+  let result = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join('');
+
+  galleryList.innerHTML = result;
+}
+
+loadGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (saveMessage) {
+    refs.textarea.value = saveMessage['message'] || '';
+    refs.input.value = saveMessage['email'] || '';
+  }
+}
+
+// function onTextareaInput(evt) {
+//   const message = evt.target.value;
+//   localStorage.setItem(STORAGE_KEY, message);
+// }
